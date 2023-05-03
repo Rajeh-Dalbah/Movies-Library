deb5b85235c57b9cfaceb35665d213f19c46dde6
rajeh-dalbah <rajeh.dalbah@gmail.com>
2023-05-03 19:52:16 +0000

    assignment 14

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 
 ## WRRC
 ![alt text](./WRRC.png)
+![alt text](./WRRClab2.png)
 
 ## Overview
 servar to browse movies and favorites movies list.
@@ -14,9 +15,17 @@ you should Initialize by running this command "npm init -y".
 
 you should Install the required packages like : npm install express cors, to run the project.
 
+you should Install that packages to run this project its:  express, cors, axios.
+
 to start server command "npm start" so the server will run.
 
 ## Project Features
 Show movies from the server.
 
 Favorie list.
+
+Search movies.
+
+show classic movie from classic route. 
+
+you can show Robert de Niro Movies.
--- server.js
+++ server.js
@@ -3,26 +3,101 @@
     "status": 404,
     "responseText": "Sorry, page not found "
     };
+let PORT = 3000    
 const express = require('express');
 const cors = require('cors');
 const movieData = require('./Movie Data/data.json');
+const axios = require('axios');
+require('dotenv').config();
+const APIKEY = process.env.APIKEY;
+
 
 const server = express();
 server.use(cors());
 
 server.get('/',handelHome);
 server.get('/favorite',handelFavorite);
+server.get('/trending',handeltrending);
+server.get('/search',handelsearch);
+server.get('/classic', handelOldMovies);
+server.get('/actor', handleByActor);
 server.get('*',handelNotFound);
 server.use(errorHandler);
 
+let url =`https://api.themoviedb.org/3/trending/all/week?api_key=${APIKEY}&language=en-US`;
 
-
-function Movie(title,poster_path,overview){
+function Movie(id, title,release_date , poster_path,overview){
+    this.id=id;
     this.title=title;
+    this.release_date=release_date;
     this.poster_path=poster_path;
     this.overview=overview;
    
  }
+ 
+function handelsearch(req,res){
+    let userSearch= "interstellar";
+    let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.APIKEY}&language=en-US&query=${userSearch}`;
+
+    axios.get(url)
+    .then(results=>{
+        let movies = results.data.results.map(val =>{
+            return new Movie(val.id, val.title, val.release_date, val.poster_path, val.overview);
+        });
+        res.status(200).send(movies);  
+     }).catch(err=>{
+        handelservererror(err,req,res);
+    })} 
+
+function handelOldMovies(req,res){
+    axios.get(`http://api.themoviedb.org/3/discover/movie?api_key=${process.env.APIKEY}&year=1994`)
+    .then(result => {
+        let mapResult = result.data.results.map(val => {
+            let movie = new Movie(val.id, val.title, val.release_date, val.poster_path, val.overview);
+            return movie;
+        })
+        res.send(mapResult)
+
+    }).catch((err)=>{
+        handelservererror(err,req,res);
+          
+    })
+
+}
+
+function handleByActor(req,res){
+    axios.get(`http://api.themoviedb.org/3/discover/movie?api_key=${process.env.APIKEY}&with_cast=380`)
+    .then(result => {
+        let mapResult = result.data.results.map(val => {
+            let movie = new Movie(val.id, val.title, val.release_date, val.poster_path, val.overview);
+            return movie;
+        })
+        res.send(mapResult)
+
+    }).catch((err)=>{
+        handelservererror(err,req,res);
+          
+    })
+
+
+}
+
+
+function handeltrending(req,res){
+    axios.get(url)
+    .then(result => {
+        let mapResult = result.data.results.map(val => {
+            let movie = new Movie(val.id, val.title, val.release_date, val.poster_path, val.overview);
+            return movie;
+        })
+        res.send(mapResult)
+
+    }).catch((err)=>{
+        handelservererror(err,req,res);
+          
+    })
+}
+ 
 
 function handelHome(req,res){
         let obj ;
@@ -46,5 +121,5 @@ function errorHandler (error,req,res){
     res.status(500).send(err);
 }
 
-server.listen(3000, ()=>{
+server.listen(PORT, ()=>{
     console.log("listinig to port 3000");})
